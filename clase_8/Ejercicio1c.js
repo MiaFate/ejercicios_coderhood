d097da33432d04e9b4ec266702211a546d249818
MiaFate <miapurplefroot@gmail.com>
2021-07-13 19:26:43 -0300

    agregado try-catch al ejercicio 1c

--- clase_8/Ejercicio1c.js
+++ clase_8/Ejercicio1c.js
@@ -1,20 +1,24 @@
 /* Reescribir la función programa del insiso anterior para que use async y await */
 
 function dividirNumerosAsync(dividendo, divisor) {
-    return new Promise(function (resolve, reject) {
-      try {
-        setTimeout(() => {
-          resolve(dividendo / divisor);
-        }, 3000);
-      } catch (error) {
-        reject(error);
-      }
-    });
+  return new Promise(function (resolve, reject) {
+    try {
+      setTimeout(() => {
+        resolve(dividendo / divisor);
+      }, 3000);
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
+
+async function programa(a, b) {
+  try {
+    const resultado = await dividirNumerosAsync(a, b);
+    console.log("el resultado es:", resultado);
+  } catch (error) {
+    console.log(error);
   }
-  
-  async function programa(a, b) {
-    const resultado=await dividirNumerosAsync(a, b)
-    console.log('el resultado es:', resultado) 
-  }
-  
-  programa(10, 5)
+}
+
+programa(10, 5);
